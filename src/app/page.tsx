7c195310ad9ba82c2bcc402b55d30f55e451989e
Francisco Villella <franciscovillella2003@gmail.com>
2024-04-04 20:41:11 -0300

    Fix margen top del banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Notificaciones } from "./components/Notificaciones";
 export default function Home() {
   return (
     <div className="w-full flex flex-col ml-[350px]">
-      <div className="flex justify-center h-[30px] bg-yellow-400 my-5 rounded-[8px] text-black text-[20px] font-semibold pb-10 py-2">
+      <div className="flex justify-center h-[30px] bg-yellow-400  rounded-[8px] text-black text-[20px] font-semibold pb-10 py-2">
         Este es un sitio ficticio realizado por mi, no es la página oficial de
         Takenos. Visite Takenos
         <a
